d6a50c2246f094a7b51f4c173724419083d6589d
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2023-07-05 10:28:59 +0200

    rename and refactor BookingStateMachineConfig

--- src/main/java/com/fhdo/bookingservice/config/BookingStateMachineConfig.java
+++ src/main/java/com/fhdo/bookingservice/config/BookingStateMachineConfig.java
@@ -13,11 +13,13 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 import java.util.EnumSet;
-
+/*
+* https://docs.spring.io/spring-statemachine/docs/3.2.x/reference/#sm-config
+* */
 @Slf4j
 @EnableStateMachineFactory
 @Configuration
-public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<BookingState, BookingEvent> {
+public class BookingStateMachineConfig extends EnumStateMachineConfigurerAdapter<BookingState, BookingEvent> {
     @Override
     public void configure(StateMachineStateConfigurer<BookingState, BookingEvent> states) throws Exception {
         states.withStates()
@@ -29,25 +31,65 @@ public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<Bookin
 
     @Override
     public void configure(StateMachineTransitionConfigurer<BookingState, BookingEvent> transitions) throws Exception {
-        transitions.withExternal().source(BookingState.NEW).target(BookingState.CONFIRMED).event(BookingEvent.CONFIRMATION_APPROVED)
+        transitions
+                .withExternal()
+                .source(BookingState.NEW)
+                .target(BookingState.CONFIRMED)
+                .event(BookingEvent.CONFIRMATION_APPROVED)
                 .and()
-                .withExternal().source(BookingState.NEW).target(BookingState.DECLINED).event(BookingEvent.CONFIRMATION_DECLINED)
+
+                .withExternal()
+                .source(BookingState.NEW)
+                .target(BookingState.DECLINED)
+                .event(BookingEvent.CONFIRMATION_DECLINED)
                 .and()
-                .withExternal().source(BookingState.NEW).target(BookingState.CANCELLED).event(BookingEvent.BOOKING_CANCELLED)
+
+                .withExternal()
+                .source(BookingState.NEW)
+                .target(BookingState.CANCELLED)
+                .event(BookingEvent.BOOKING_CANCELLED)
                 .and()
-                .withExternal().source(BookingState.CONFIRMED).target(BookingState.ACTIVE).event(BookingEvent.VEHICLE_PARKED)
+
+                .withExternal()
+                .source(BookingState.CONFIRMED)
+                .target(BookingState.ACTIVE)
+                .event(BookingEvent.VEHICLE_PARKED)
                 .and()
-                .withExternal().source(BookingState.CONFIRMED).target(BookingState.CANCELLED).event(BookingEvent.BOOKING_CANCELLED)
+
+                .withExternal()
+                .source(BookingState.CONFIRMED)
+                .target(BookingState.CANCELLED)
+                .event(BookingEvent.BOOKING_CANCELLED)
                 .and()
-                .withExternal().source(BookingState.ACTIVE).target(BookingState.OVERSTAY).event(BookingEvent.OVERSTAY_OCCURRED)
+
+                .withExternal()
+                .source(BookingState.ACTIVE)
+                .target(BookingState.OVERSTAY)
+                .event(BookingEvent.OVERSTAY_OCCURRED)
                 .and()
-                .withExternal().source(BookingState.ACTIVE).target(BookingState.PENDING_EXTENSION).event(BookingEvent.EXTENSION_REQUESTED)
+
+                .withExternal()
+                .source(BookingState.ACTIVE)
+                .target(BookingState.PENDING_EXTENSION)
+                .event(BookingEvent.EXTENSION_REQUESTED)
                 .and()
-                .withExternal().source(BookingState.ACTIVE).target(BookingState.COMPLETED).event(BookingEvent.BOOKING_COMPLETED)
+
+                .withExternal()
+                .source(BookingState.ACTIVE)
+                .target(BookingState.COMPLETED)
+                .event(BookingEvent.BOOKING_COMPLETED)
                 .and()
-                .withExternal().source(BookingState.PENDING_EXTENSION).target(BookingState.ACTIVE).event(BookingEvent.EXTENSION_CONFIRMED)
+
+                .withExternal()
+                .source(BookingState.PENDING_EXTENSION)
+                .target(BookingState.ACTIVE)
+                .event(BookingEvent.EXTENSION_CONFIRMED)
                 .and()
-                .withExternal().source(BookingState.COMPLETED).target(BookingState.PAID).event(BookingEvent.PAYMENT_SUCCESSFUL);
+
+                .withExternal()
+                .source(BookingState.COMPLETED)
+                .target(BookingState.PAID)
+                .event(BookingEvent.PAYMENT_SUCCESSFUL);
     }
 
     @Override
