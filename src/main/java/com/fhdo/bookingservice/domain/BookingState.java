cfdcd4d17a3f87e6e9814106cfe14498573c1b7a
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2023-07-07 02:59:07 +0200

    remove payment from state machine

--- src/main/java/com/fhdo/bookingservice/domain/BookingState.java
+++ src/main/java/com/fhdo/bookingservice/domain/BookingState.java
@@ -7,7 +7,8 @@ package com.fhdo.bookingservice.domain;
  *
  * <p>Possible states include:
  * <ul>
- *   <li>{@link BookingState#NEW}: The booking request has been received but not processed yet.</li>
+ *   <li>{@link BookingState#NEW}: The booking request has been received and a draft booking is created.</li>
+ *   <li>{@link BookingState#PENDING_CONFIRMATION}: The user has confirmed his booking intention, booking is pending check with parking service.</li>
  *   <li>{@link BookingState#CONFIRMED}: The booking has been successfully confirmed with parking service.</li>
  *   <li>{@link BookingState#DECLINED}: The booking failed to get confirmation with parking service, the user has to request new booking</li>
  *   <li>{@link BookingState#ACTIVE}: The booking is currently active, and the user has parked their vehicle.</li>
@@ -15,17 +16,16 @@ package com.fhdo.bookingservice.domain;
  *   <li>{@link BookingState#OVERSTAY}: The user has exceeded the allowed booking duration, and additional charges or penalties may apply.</li>
  *   <li>{@link BookingState#COMPLETED}: The booking has been successfully completed, and the user has left the parking spot.</li>
  *   <li>{@link BookingState#CANCELLED}: The booking has been canceled by the user or the system.</li>
- *   <li>{@link BookingState#PAID}: The booking was paid by user and is considered in a closed state.</li>
  * </ul>
  */
 public enum BookingState {
     NEW,
+    PENDING_CONFIRMATION,
     CONFIRMED,
     DECLINED,
     ACTIVE,
     PENDING_EXTENSION,
     OVERSTAY,
     COMPLETED,
-    CANCELLED,
-    PAID
+    CANCELLED
 }
