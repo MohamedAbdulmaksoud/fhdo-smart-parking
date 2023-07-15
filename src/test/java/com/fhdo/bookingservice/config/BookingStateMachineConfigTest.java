45bc9533c27b80e8f07b404b3b0974fabaf2ec3b
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2023-07-15 17:35:11 +0200

    update BookingStateMachineConfigTest to support the bookingConfirmationRequest

--- src/test/java/com/fhdo/bookingservice/config/BookingStateMachineConfigTest.java
+++ src/test/java/com/fhdo/bookingservice/config/BookingStateMachineConfigTest.java
@@ -2,14 +2,19 @@ package com.fhdo.bookingservice.config;
 
 import com.fhdo.bookingservice.domain.BookingEvent;
 import com.fhdo.bookingservice.domain.BookingState;
+import com.fhdo.bookingservice.domain.request.BookingConfirmationRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import reactor.core.publisher.Mono;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @SpringBootTest
 class BookingStateMachineConfigTest {
@@ -20,12 +25,23 @@ class BookingStateMachineConfigTest {
     @Test
     void testBookingStateMachine() {
         StateMachine<BookingState, BookingEvent> sm = factory.getStateMachine();
+        BookingConfirmationRequest request = BookingConfirmationRequest.builder()
+                .bookingId(UUID.randomUUID())
+                .userId(UUID.randomUUID())
+                .parkingLotId(UUID.randomUUID())
+                .startTime(OffsetDateTime.now())
+                .endTime(OffsetDateTime.now().plusMinutes(30))
+                .build();
+
         sm.startReactively().subscribe();
-        sm.sendEvent(Mono.just(MessageBuilder
-                .withPayload(BookingEvent.BOOKING_CONFIRMED)
-                .build()))
-                .subscribe();
-        Assertions.assertEquals(BookingState.CONFIRMED, sm.getState().getId());
+
+        Message<BookingEvent> msg = MessageBuilder.withPayload(BookingEvent.CHECK_VALIDITY)
+                .setHeader("booking_id", request.getBookingId())
+                .setHeader(request.getHeader(),request)
+                .build();
+
+        sm.sendEvent(Mono.just(msg)).subscribe();
+        Assertions.assertEquals(BookingState.PENDING_CONFIRMATION, sm.getState().getId());
 
     }
 
