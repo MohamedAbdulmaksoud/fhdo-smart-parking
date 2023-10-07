15e0389da0bebd87ec80ec043bf7cbaf534b414a
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2023-10-07 04:15:39 +0200

    refactor BookingEntityMapper

--- src/main/java/com/fhdo/bookingservice/mappers/BookingEntityMapper.java
+++ src/main/java/com/fhdo/bookingservice/mappers/BookingEntityMapper.java
@@ -1,13 +1,13 @@
 package com.fhdo.bookingservice.mappers;
 
-import com.fhdo.bookingservice.domain.request.BookingConfirmationRequest;
+import com.fhdo.bookingservice.domain.request.BookingConfirmationMessageRequest;
 import com.fhdo.bookingservice.domain.response.BookingConfirmationResponse;
 import com.fhdo.bookingservice.entities.BookingEntity;
 import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BookingEntityMapper {
-    BookingConfirmationRequest bookingToConfirmationRequest(BookingEntity bookingEntity);
+    BookingConfirmationMessageRequest bookingToConfirmationRequest(BookingEntity bookingEntity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     BookingEntity update(BookingConfirmationResponse bookingValidationRequest, @MappingTarget BookingEntity bookingEntity);
