d818b638b08409b3155daae043813dde1c50b427
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2023-10-10 13:49:57 +0200

    add bookingToCancellationRequest mapper

--- src/main/java/com/fhdo/bookingservice/mappers/BookingEntityMapper.java
+++ src/main/java/com/fhdo/bookingservice/mappers/BookingEntityMapper.java
@@ -1,5 +1,6 @@
 package com.fhdo.bookingservice.mappers;
 
+import com.fhdo.bookingservice.domain.request.BookingCancellationMessageRequest;
 import com.fhdo.bookingservice.domain.request.BookingConfirmationMessageRequest;
 import com.fhdo.bookingservice.domain.response.BookingConfirmationResponse;
 import com.fhdo.bookingservice.entities.BookingEntity;
@@ -8,6 +9,8 @@ import org.mapstruct.*;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BookingEntityMapper {
     BookingConfirmationMessageRequest bookingToConfirmationRequest(BookingEntity bookingEntity);
+
+    BookingCancellationMessageRequest bookingToCancellationRequest(BookingEntity bookingEntity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     BookingEntity update(BookingConfirmationResponse bookingValidationRequest, @MappingTarget BookingEntity bookingEntity);
